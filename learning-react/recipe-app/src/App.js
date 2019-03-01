26ccf9d1febd258368665ae64fcd99fc0d243aeb
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-03-01 11:22:59 +0600

    old react code

--- learning-react/recipe-app/src/App.js
+++ learning-react/recipe-app/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import { render } from 'react-dom'
+import Menu from './components/Menu'
+import data from '../data/recipes'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Recipe app</h1>
+        <Menu recipes={data} />
       </div>
     );
   }
---
+++ learning-react/recipe-app/src/components/Ingredient.js
@@ -0,0 +1,13 @@
+const Ingredient = (amount, measurement, name ) => {
+    return (
+        <li>
+            <span className="amount">{amount}</span>
+            <span className="measurement">{measurement}</span>
+            <span className="name">{name}</span>
+        </li>
+        )
+}
+
+Ingredient.displayName = 'Ingredient'
+
+export default Ingredient
