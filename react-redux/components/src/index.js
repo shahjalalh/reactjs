30ae5f73d182a49624b92cadf2371d070cfd646e
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-03-06 22:13:55 +0600

    components

--- react-redux/components/src/index.js
+++ react-redux/components/src/index.js
@@ -8,6 +8,14 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
+
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
             <ApprovalCard>
                 <CommentDetail
                     author="Alex"
