674e83c0eb35f622c8bc1e14bd9fc7faa00a58d4
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-03-09 13:08:27 +0600

    pics

--- react-redux/pics/src/components/ImageList.js
+++ react-redux/pics/src/components/ImageList.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
 
     const images = props.images.map((image) => {
-        return (<img src={image.urls.regular} />);
+        return (
+            <ImageCard key={image.id} image={image} />
+        );
     });
 
     return (
-        <div>
+        <div className="image-list">
             {images}
         </div>
     );
