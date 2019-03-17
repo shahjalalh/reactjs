8340545caae774b5aa9df5353fac9f7c7a0e8f40
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-03-17 20:55:24 +0600

    blog

--- react-redux/blog/src/components/PostList.js
+++ react-redux/blog/src/components/PostList.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component{
+
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
 
     render(){
 
@@ -12,4 +18,4 @@ class PostList extends React.Component{
     }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList);
