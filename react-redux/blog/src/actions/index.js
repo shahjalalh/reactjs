1c0819289b3d6b6d15f01c4c1647e8c9d68b8b85
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-03-22 21:01:39 +0600

    memoize version of fetch

--- react-redux/blog/src/actions/index.js
+++ react-redux/blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
@@ -5,9 +6,25 @@ export const fetchPosts = () => async dispatch => {
 
     dispatch({
         type: 'FETCH_POSTS',
-        payload: response
+        payload: response.data
     });
-
 };
 
+
+/*
+// memoize version of fetch post
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch);
+};
+
+// private function
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
+});
+*/
 
