17a91ef0fcf4e7763be39b55eb8d666ce29bab22
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-03-31 21:58:52 +0600

    app

--- react-redux/streams/client/src/components/App.js
+++ react-redux/streams/client/src/components/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
-const PageOne = () => {
-    return (
-        <div>
-            PageOne <br/>
-            <Link to="/pagetwo">Navigate to Page Two</Link>
-        </div>
-    );
-}
-
-const PageTwo = () => {
-    return (
-        <div>
-            PageTwo <br/>
-            <Link to="/">Navigate to Page One</Link>
-        </div>
-    );
-}
 
 const App = () => {
     return (
