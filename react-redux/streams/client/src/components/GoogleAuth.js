529160d9afb920b6e93553cd57f908118044504f
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-04-22 09:36:42 +0600

    streams

--- react-redux/streams/client/src/components/GoogleAuth.js
+++ react-redux/streams/client/src/components/GoogleAuth.js
@@ -33,5 +33,40 @@ class GoogleAuth extends React.Component{
     onSignOutClick = () => {
         this.auth.signOut();
     };
+
+    renderAuthButton(){
+        if(this.props.isSignedIn === null){
+            return null;
+        }else if(this.props.isSignedIn){
+            return (
+                <button onClick={this.onSignOutClick} className="ui red google button">
+                    <i className="google icon" /> 
+                    Sign Out
+                </button>
+            );
+        }else{
+            return (
+                <button onClick={this.onSignInClick} className="ui red google button">
+                    <i className="google icon"/>
+                    Sign In with Google
+                </button>
+            );
+        }
+    }
+
+    render(){
+        return (
+            <div>{this.renderAuthButton()}</div>
+        );
+    }
 }
+
+const mapStateToProps = state => {
+    return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(
+    mapStateToProps,
+    { signIn, signOut }
+)(GoogleAuth);
 
