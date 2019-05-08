3536a4d8fdc6c0689e1babdee1e6df41a5941f0a
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-05-08 20:01:55 +0600

    hooks simple

--- react-redux/hooks-simple/src/components/App.js
+++ react-redux/hooks-simple/src/components/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 const App = () => {
+    const [resource, setResource] = useState('posts');
 
     return (
         <div>
-            App
+            <userList />
+            <div>
+                <button onClick={()=> setResource('posts')}>Posts</button>
+                <button onClick={()=> setResource('todos')}>Todos</button>
+            </div>
+            <ResourceList resource={resource} />
         </div>
     );
     
